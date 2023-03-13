ae62c2ab3ac2310e008624b13c161ab83f760dae
Imbeshat <md.imbeshat@gmail.com>
2023-03-13 23:29:29 +0530

    fixed console error for id

--- src/Components/MealItem.js
+++ src/Components/MealItem.js
@@ -8,7 +8,7 @@ const MealItem = ({ data }) => {
 				? "Not Found"
 				: data.map((item) => {
 						return (
-							<div className="w-auto rounded-2xl bg-slate-800 p-2">
+							<div key={item.idMeal} className="w-auto rounded-2xl bg-slate-800 p-2">
 								<img src={item.strMealThumb} alt="meal" className="rounded-2xl" />
 								<h3 className="text-white text-lg">{item.strMeal}</h3>
 								<IngredientsPopup data={item} />
